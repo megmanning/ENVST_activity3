e7999c3b8d16e9a4acd2fbded50968203aa3bbed
Meg Manning <mmanning@hamilton.edu>
2020-09-17 16:09:42 -0400

    all of activity 3

--- activity3.R
+++ activity3.R
@@ -16,3 +16,43 @@ bartlett.test(ch4$CH4_Flux ~ ch4$herbivory)
 
 #we've confirmed we can use t-test for data
 t.test(ch4$CH4_Flux ~ ch4$herbivory)
+
+#one sample t-test 
+help("t.test")
+
+#read in insect data
+datI <- read.csv("/Users/margaretmanning/Desktop/Data Science/a03/insect_richness.csv")
+
+datI$urbanName <- as.factor(datI$urbanName)
+
+#running an ANOVA test using lm and aov
+#specifiy model for species richness and urban type
+in.mod <- lm(datI$Richness ~ datI$urbanName)
+#run the ANOVA
+in.aov <- aov(in.mod)
+#print out ANOVA table
+summary(in.aov)
+
+#run Tukey HSD (compares differences btw grps)
+tukeyT <- TukeyHSD(in.aov)
+#view results
+tukeyT
+
+#make a plot to visualize Tukey
+#make axes labels smaller than usual to fit on plot using cex.axis
+plot(tukeyT, cex.axis=0.75)
+
+#calculate means across factor data 
+tapply(datI$Richness, datI$urbanName, "mean")
+
+#set up contingency table
+species <- matrix(c(18,8,15,32), ncol = 2, byrow = TRUE)
+colnames(species) <- c("Not Protected", "Protected")
+rownames(species) <- c("Declining", "Stable/Increase")
+
+#make a mosaic plot with an informative title and axes labels 
+mosaicplot(species, xlab="Population Status", ylab="Legal Protection",
+           main="Legal Protection Impacts on Populations")
+
+#Conduct a chi-squared test 
+chisq.test(species)
